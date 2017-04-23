0e833c753534feb09db7c1694be396aa634bf04d
Aditya Ekawade <aditya.ekawade@gmail.com>
2017-04-23 16:50:32 -0600

    with first example

---
+++ autocompletedemo.js
@@ -0,0 +1,17 @@
+var Autocomplete = require('Autocomplete');
+
+
+var VEGETABLES = ['arugula', 'beet', 'broccoli', 'cauliflower', 'corn', 'cabbage', 'carrot'];
+
+// Create the autocomplete object
+var autocomplete = Autocomplete.connectAutocomplete();
+
+// Initialize the autocopmlete object and define a 
+// callback to populate it with data
+autocomplete.initialize(function(onReady) {
+    onReady(VEGETABLES);
+});
+
+// Later...  When it's time to search:
+var matches = autocomplete.search('ca');
+console.log(matches);
